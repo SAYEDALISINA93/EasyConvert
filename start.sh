1dcc356f171dacfa071f1dfe7ad9398092634a37
Alisina <sayedalisinaa@gmail.com>
2026-07-31 15:22:01 +0200

    Rename references from Docbench to EASYConvert in HTML and startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 cd "$(dirname "$0")/backend"
-echo "Starting Docbench at http://127.0.0.1:8000"
+echo "Starting EASYConvert at http://127.0.0.1:8000"
 uvicorn main:app --host 127.0.0.1 --port 8000
